850ac1c2427f093bfd0514d900f6d7c79ea2ac54
Samuel Wilson <samWson@users.noreply.github.com>
2024-02-09 18:01:16 +1300

    Create game loop

--- missile.exs
+++ missile.exs
@@ -2,8 +2,20 @@
 
 defmodule Game do
   def main() do
-    input = IO.gets("Enter something> ")
+    loop(%{})
+  end
+
+  defp loop(%{input: "quit"}) do
+    exit(:normal)
+  end
+
+  defp loop(state) do
+    input = IO.gets("Enter something> ") |> String.trim()
     IO.puts("You entered: #{input}")
+
+    new_state = Map.put(state, :input, input)
+
+    loop(new_state)
   end
 end
 
